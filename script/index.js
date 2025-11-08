37c0622ca083cdffd2c468e9b0b7173b350575fb
aflazur <mdaflazurrahaman2022@gmail.com>
2025-11-09 01:01:29 +0600

    Complete videos load section using API

--- script/index.js
+++ script/index.js
@@ -9,6 +9,12 @@ function loadCategories() {
         .then((data) => displayCategories(data.categories));
 }
 
+function loadVideos() {
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+        .then((response) => response.json())
+        .then((data) => displayVideos(data.videos));
+}
+
 function displayCategories(categories) {
     // console.log(categories);
 
@@ -31,4 +37,39 @@ function displayCategories(categories) {
 
 }
 
+const displayVideos = (videos) => {
+    // console.log(videos);
+    const videoContainer = document.getElementById("video-container")
+    videos.forEach(video => {
+        // console.log(video);
+        const videoCard = document.createElement("div");
+
+        videoCard.innerHTML = `
+         <div class="card bg-base-100 ">
+            <figure class="relative">
+                <img class="w-full h-[200px] object-cover" src="${video.thumbnail}" alt="Shoes" />
+                <span class="absolute bottom-2 right-2 text-white bg-black text-sm px-2 rounded">3hrs 56 min ago</span>
+            </figure>
+            <div class=" flex gap-3 px-0 py-5">
+                <div class="profile">
+                    <div class="avatar">
+                        <div class="ring-primary ring-offset-base-100 w-[40px] rounded-full">
+                            <img src="${video.authors[0].profile_picture}" />
+                        </div>
+                    </div>
+                </div>
+                <div class="intro">
+                    <h2 class="text-sm font-semibold mb-[10px]">Midnight Serenade</h2>
+                    <p class="text-sm text-gray-400 flex gap-1 mb-[10px]">${video.authors[0].profile_name} <img class="w-5 h-5" src="https://img.icons8.com/?size=32&id=6xO3fnY41hu2&format=png" alt=""></p>
+
+                    <p class="text-sm text-gray-400">${video.others.views}</p>
+                </div>
+            </div>
+        </div>
+        `
+        videoContainer.append(videoCard);
+    });
+};
+
 loadCategories();
+loadVideos();
