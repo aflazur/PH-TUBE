b7017a21932d56befc7c257ffc4d1073ce898db3
aflazur <mdaflazurrahaman2022@gmail.com>
2025-11-09 19:23:42 +0600

    Complete show details part

--- script/index.js
+++ script/index.js
@@ -42,6 +42,37 @@ const loadCategoryVideos = (id) => {
 
 }
 
+const loadVideoDetails = (videoId) => {
+    // console.log(videoId);
+   const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`
+
+   fetch(url)
+   .then(res => res.json())
+   .then (data => displayVideoDetails(data.video))
+};
+
+const displayVideoDetails = (video) => {
+    document.getElementById("video_details").showModal();
+    const detailsContainer = document.getElementById("details-container");
+    detailsContainer.innerHTML=
+    `
+    <div class="card bg-base-100 image-full w-full shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>Description : ${video.description}</p>
+    <div class="card-actions justify-end">
+    
+    </div>
+  </div>
+</div>
+    `
+}
+
 function displayCategories(categories) {
     // console.log(categories);
 
@@ -104,6 +135,7 @@ const displayVideos = (videos) => {
                     <p class="text-sm text-gray-400">${video.others.views}</p>
                 </div>
             </div>
+            <button onclick = loadVideoDetails('${video.video_id}') class="btn btn-block">Show Details</button>
         </div>
         `
         videoContainer.append(videoCard);
